9b6db6702b24a13ebb78e105ff775a9c3011553c
Friedrich von Never <friedrich@fornever.me>
2015-04-24 13:36:33 +0600

    Add EditorConfig.

--- init.el
+++ init.el
@@ -42,6 +42,7 @@
                      batch-mode
                      coffee-mode
                      cil-mode
+                     editorconfig
                      elixir-mode
                      fsharp-mode
                      gradle-mode
